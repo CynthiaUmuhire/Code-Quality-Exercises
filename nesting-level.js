5bbbf3d0feffc05bd2957b62534ed02b3bbf1fe1
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-10-25 10:44:52 +0200

    feat: add the nesting levels function

---
+++ nesting-level.js
@@ -0,0 +1,20 @@
+function findNestingLevels(array) {
+    let count = 0;
+
+    array.forEach((arrayItem) => {
+        //check if the arrayItem is an array
+        if (Array.isArray(arrayItem)) {
+            let tempCount = findNestingLevels(arrayItem);
+            if (tempCount > count) {
+                count = tempCount;
+            }
+        }
+    })
+    return count + 1;
+}
+function flattenArray(array) {
+    const nestingLevel = findNestingLevels(array)
+    return array.flat(nestingLevel)
+}
+
+console.log(flattenArray([[[1, 2, 3, 4], 4],6,[8,9]]))
